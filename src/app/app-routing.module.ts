996389a24bb6fcc25a084ae6d701e9f6d763e219
kpsr363636 <36java36@gmail.com>
2022-01-04 11:11:43 +0530

    1 file modified, 4 added components created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import{ RouterModule, Routes } from '@angular/router'
 import { EmployeeregisterComponent } from './employeeregister/employeeregister.component';
+import { EmployeeallComponent } from './employeeall/employeeall.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'employeeregister', pathMatch:'full'}, 
   {path:'employeeregister', component:EmployeeregisterComponent},
+  {path:'employeeallComponent', component:EmployeeallComponent},
 ]
 
 @NgModule({
---
+++ src/app/employeeall/employeeall.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employeeall',
+  templateUrl: './employeeall.component.html',
+  styleUrls: ['./employeeall.component.css']
+})
+export class EmployeeallComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

929774eb3b99e082540c66b050c8adc997cda285
kpsr363636 <36java36@gmail.com>
2022-01-05 09:49:39 +0530

    3 files modified, 10 added, Tried by me with out help nagireddy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import{ RouterModule, Routes } from '@angular/router'
-
+import { RouterModule, Routes } from '@angular/router';
+import { EmployeeAllComponent } from './employee-all/employee-all.component';
+import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
+import { EmployeeRegisterComponent } from './employee-register/employee-register.component';
 
 const routes: Routes = [
-  {path:'', redirectTo:'employeeallComponent', pathMatch:'full'}, 
-  // {path:'employeeallComponent', component:EmployeeallComponent},
-  // {path:'employeeregister', component:EmployeeregisterComponent},
- 
-]
+  //{path:'', redirectTo:'employeeallComponent', pathMatch:'full'},
+  { path: 'list', component: EmployeeAllComponent },
+  { path: 'edit/:id', component: EmployeeEditComponent },
+  { path: 'add', component: EmployeeRegisterComponent },
+  { path: '', redirectTo: 'list', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
- 
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { EmployeeAllComponent } from './employee-all/employee-all.component';
+import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
+import { EmployeeRegisterComponent } from './employee-register/employee-register.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
-  declarations: [ AppComponent, HelloComponent,  ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule,HttpClientModule ],
+  declarations: [ AppComponent, HelloComponent,EmployeeAllComponent,EmployeeEditComponent, EmployeeRegisterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
